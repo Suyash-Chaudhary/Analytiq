3f556eb921e946c9fb72e83cd82ee1377e9efb16
Suyash-Chaudhary <cout.suyash@gmail.com>
2024-09-18 01:46:17 +0530

    Changed instance methods to member functions

--- shared/src/clients/redis/pub-sub/redis-pub-sub-publisher.ts
+++ shared/src/clients/redis/pub-sub/redis-pub-sub-publisher.ts
@@ -1,23 +1,12 @@
 import { RedisClientType } from "redis";
 import { CustomEvent } from "../../../events/custom-event";
 
-export abstract class RedisPSSubscriber<EventType extends CustomEvent> {
+export abstract class RedisPSPublisher<EventType extends CustomEvent> {
   abstract get subject(): EventType["subject"];
-  abstract validator: (payload: any) => EventType["data"];
-  abstract handler: (payload: EventType["data"]) => Promise<void>;
   protected constructor() {}
 
-  subscribe(client: RedisClientType) {
-    client.subscribe(this.subject, async (message) => {
-      try {
-        const payload = JSON.parse(message);
-        const data = this.validator(payload);
-        await this.handler(data);
-      } catch (error) {
-        throw new Error(
-          `ERROR: Unable to process message.\n Channel: ${this.subject}\n\n Message: ${message}\n\n ERROR: ${error}`
-        );
-      }
-    });
+  async publish(client: RedisClientType, data: EventType["data"]) {
+    const message = JSON.stringify(data);
+    await client.publish(this.subject, message);
   }
 }
